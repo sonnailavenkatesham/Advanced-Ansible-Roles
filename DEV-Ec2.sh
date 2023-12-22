299e68244b925ace31493f960d412924412b5d86
Roman Reings <Rajkumar>
2023-12-22 13:37:59 +0530

    added files

--- DEV-Ec2.sh
+++ DEV-Ec2.sh
@@ -4,13 +4,13 @@ INSTANCE_TYPE=t2.micro
 SECURITY_GROUP=sg-001f7a94d24250a78
 HOTSTED_ZONE_ID=Z05444171VES3WQ8S22YW
 DOMAIN_NAME=venkateshamsonnalia143.online
-NAME=$("mongodb", "catalogue", "cart", "web", "user", "shipping")
+NAME=$@
 for i in $NAME
 do 
     echo " Name: $i "
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
-    echo " $i-IP:$IP_ADDRESS "
+    echo " Name:$i and IP:$IP_ADDRESS "
     aws route53 change-resource-record-sets --hosted-zone-id $HOTSTED_ZONE_ID --change-batch '{
             "Comment": "CREATE a record ",
             "Changes": [{
